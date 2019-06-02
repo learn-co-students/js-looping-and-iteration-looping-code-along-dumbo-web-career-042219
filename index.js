d63723685c90329a7516a4f370581faf45250d33
Aaron Chu <aaronchu357@gmail.com>
2019-06-02 13:28:42 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+function writeCards(arr, event) {
+  let output = []
+  for(let i = 0; i < arr.length; i++){
+    output.push(`Thank you, ${arr[i]}, for the wonderful ${event} gift!`)
+  }
+  return output
+}
+
+function countdown(int) {
+  while(int > -1) {
+    console.log(int);
+    int--;
+  }
+
+}
