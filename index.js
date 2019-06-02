505bd146a5f68cb7d0a4c86c6208453c6b4b56c8
Miles Marillo <mjm03@hampshire.edu>
2019-06-02 16:17:00 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+function writeCards(names, event) {
+	let cards = []
+for (let i = 0; i < names.length; i++) {
+cards.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+}
+return cards
+}
+
+function countdown(theNumber) {
+while (theNumber > 0 ) {
+	console.log( theNumber );
+	theNumber -= 1;
+}
+console.log( theNumber )
+}
