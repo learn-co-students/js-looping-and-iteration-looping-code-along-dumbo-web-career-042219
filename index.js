ec066369a8445e366061e3c6361783cb68269501
Wei <theweifeng@gmail.com>
2019-06-03 09:29:57 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+
+function writeCards(arr, string){
+  let new_arr = [];
+  for (let i = 0; i < arr.length; i++){
+    new_arr.push(`Thank you, ${arr[i]}, for the wonderful ${string} gift!`);
+  }
+
+  return new_arr;
+}
+
+function countdown(){
+  let i = 10;
+  
+  while (i >= 0) {
+    console.log(i--);
+  }
+}
